1f46ba90f42e66e72a1512ebd458168808fe4e1b
Julian Bonilla <julianjbonilla@gmail.com>
2012-01-12 15:52:07 -0500

    First commit.

---
+++ src/main/java/gov/miamidade/portlet/HomeController.java
@@ -0,0 +1,40 @@
+package gov.miamidade.portlet;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+/**
+ * Handles requests for the portlet view mode.
+ */
+@Controller
+@RequestMapping("VIEW")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RenderMapping
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! the client locale is "+ locale.toString());
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate);
+		
+		return "home";
+	}
+	
+}
